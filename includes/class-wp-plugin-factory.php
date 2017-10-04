94a0fe61e8706fa985f9840029a564fa1973374e
Gabor Javorszky <gabor@javorszky.co.uk>
2017-10-04 15:41:21 +0100

    Add details to dockblock about factory file

--- includes/class-wp-plugin-factory.php
+++ includes/class-wp-plugin-factory.php
@@ -2,6 +2,11 @@
 
 /**
  * Plugin factory, so we get one shared instance of the class that's not a singleton.
+ *
+ * @since     1.0.0
+ * @author    Mindsize <info@mindsize.me>
+ * @copyright Copyright (c) 2017 Mindsize <info@mindsize.me>
+ * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License v2.0
  */
 class WP_Plugin_Factory {
 	public static function create() {
