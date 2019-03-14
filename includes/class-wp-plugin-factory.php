23b8740ee5aff91b366334d205aaa44fdb9329e6
Gabor Javorszky <gabor@javorszky.co.uk>
2019-03-14 12:44:32 +0000

    Rework class-plugin-factory.php for phpcs

--- includes/class-wp-plugin-factory.php
+++ includes/class-wp-plugin-factory.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * File that holds the WP Plugin Factory.
+ *
+ * It implements a shared instance. Functional access calls this
+ * which returns a stored instance of a plugin.
+ *
+ * @package Mindsize\WP_Plugin_Base
+ */
 namespace Mindsize\WP_Plugin_Base;
 
 /**
@@ -10,6 +18,12 @@ namespace Mindsize\WP_Plugin_Base;
  * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License v2.0
  */
 class WP_Plugin_Factory {
+	/**
+	 * Method that either creates an instance of Core, or returns the
+	 * previously created instance of Core.
+	 *
+	 * @return Core
+	 */
 	public static function create() {
 		static $plugin = null;
 
