5b6e3b7f7f10a78506bf922e932a7648d8c74d1d
Gabor Javorszky <gabor@javorszky.co.uk>
2019-03-14 12:39:44 +0000

    Remove php 5.2 support

--- wp-plugin-base.php
+++ wp-plugin-base.php
@@ -21,8 +21,8 @@ define( 'WP_PLUGIN_BASE_FILE', __FILE__ );
 define( 'WP_PLUGIN_BASE_DIR', plugin_dir_path( WP_PLUGIN_BASE_FILE ) );
 define( 'WP_PLUGIN_BASE_URL', plugin_dir_url( WP_PLUGIN_BASE_FILE ) );
 
-if ( file_exists( WP_PLUGIN_BASE_DIR . 'vendor/autoload_52.php' ) ) {
-	require_once WP_PLUGIN_BASE_DIR . 'vendor/autoload_52.php';
+if ( file_exists( WP_PLUGIN_BASE_DIR . 'vendor/autoload.php' ) ) {
+	require_once WP_PLUGIN_BASE_DIR . 'vendor/autoload.php';
 }
 
 /**
